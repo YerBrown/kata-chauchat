74bfb91b9c7d992d7a790442b6d7b66063ce9f31
YerBrown <yerbrown98@gmail.com>
2024-09-17 10:02:28 +0200

    Funcionamiento de chauchat y sus fallos

---
+++ index.js
@@ -0,0 +1,16 @@
+let cargador = ['pium!', 'pium!', 'pium!', 'pium!', 'pium!', 'pium!', 'pium!'];
+function chauchat(municion) {
+    let disparosTotales = '';
+    if (Math.random() <= 0.8) {
+        console.log('Illo, me he quedao pillá!');
+        return;
+    }
+    for(let i = 0; i < municion.length; i++){
+        disparosTotales += municion[i] + '\n';
+        if((i + 1) % 3 === 0){
+            disparosTotales += '\n';
+        }
+    }
+    console.log(disparosTotales);
+}
+chauchat(cargador);
